fe3ee12f63dc8b55ec525e3d22a6f527f5a86ca3
Eric Schreiter <mail@shter.de>
2019-01-16 15:46:37 +0100

    Innitial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,43 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-offline`,
+    {
+      const options = {
+        importWorkboxFrom: `local`,
+        globDirectory: rootDir,
+        globPatterns,
+        modifyUrlPrefix: {
+          // If `pathPrefix` is configured by user, we should replace
+          // the default prefix with `pathPrefix`.
+          "/": `${pathPrefix}/`,
+        },
+        cacheId: `gatsby-plugin-offline`,
+        // Don't cache-bust JS or CSS files, and anything in the static directory,
+        // since these files have unique URLs and their contents will never change
+        dontCacheBustUrlsMatching: /(\.js$|\.css$|static\/)/,
+        runtimeCaching: [
+          {
+            // Use cacheFirst since these don't need to be revalidated (same RegExp
+            // and same reason as above)
+            urlPattern: /(\.js$|\.css$|static\/)/,
+            handler: `cacheFirst`,
+          },
+          {
+            // Add runtime caching of various other page resources
+            urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+            handler: `staleWhileRevalidate`,
+          },
+          {
+            // Google Fonts CSS (doesn't end in .css so we need to specify it)
+            urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
+            handler: `staleWhileRevalidate`,
+          },
+        ],
+        skipWaiting: true,
+        clientsClaim: true,
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
